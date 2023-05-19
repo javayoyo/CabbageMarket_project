12928ff05a0258c1a8b0aa848ad45995de8b81d0
javayoyo <miyuzang613@gmail.com>
2023-05-19 13:07:30 +0900

    setting_틀정리중

--- src/main/java/com/icia/market/controller/BoardController.java
+++ src/main/java/com/icia/market/controller/BoardController.java
@@ -1,4 +1,134 @@
 package com.icia.market.controller;
 
+import com.icia.market.dto.BoardDTO;
+import com.icia.market.dto.BoardFileDTO;
+import com.icia.market.dto.CommentDTO;
+import com.icia.market.dto.PageDTO;
+import com.icia.market.service.BoardService;
+import com.icia.market.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
 public class BoardController {
+
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "boardPages/boardSave";
+    }
+
+    //    @PostMapping("/board/save") // /board/board/save
+    @PostMapping("/save") // /board/board/save
+    public String save(@ModelAttribute BoardDTO boardDTO) throws IOException {
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "redirect:/board/list";
+
+    }
+
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "boardPages/boardList";
+    }
+
+    @GetMapping("/paging")
+    public String paging(@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                         @RequestParam(value = "q", required = false, defaultValue = "") String q,
+                         @RequestParam(value = "type", required = false, defaultValue = "boardTitle") String type,
+                         Model model) {
+        System.out.println("page = " + page + ", q = " + q);
+        List<BoardDTO> boardDTOList = null;
+        PageDTO pageDTO = null;
+//        검색어가 없으면 일반페이징처리, 해당하면 해당건에 대한 페이징처리
+        if (q.equals("")) {
+            // 사용자가 요청한 페이지에 해당하는 글 목록 데이터
+            boardDTOList = boardService.pagingList(page);
+            // 하단에 보여줄 페이지 번호 목록 데이터  (현재 하단의 숫자 목록 갯수)
+            pageDTO = boardService.pagingParam(page);
+        } else {
+            boardDTOList = boardService.searchList(page, type, q);
+            pageDTO = boardService.pagingSearchParam(page, type, q);
+        }
+        model.addAttribute("boardList", boardDTOList);
+        model.addAttribute("paging", pageDTO);
+        model.addAttribute("q", q);
+        model.addAttribute("type", type);
+        return "boardPages/boardPaging";
+    }
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model,
+                           @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                           @RequestParam(value = "q", required = false, defaultValue = "") String q,
+
+                           @RequestParam(value = "type", required = false, defaultValue = "boardTitle") String type)
+    {
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        model.addAttribute("page",page);
+        model.addAttribute("q",q);
+        model.addAttribute("type",type);
+
+
+        if (boardDTO.getFileAttached() == 1) {
+            List<BoardFileDTO> boardFileDTO = boardService.findFile(id);
+            model.addAttribute("boardFileList", boardFileDTO);
+            System.out.println("boardFileDTO = " + boardFileDTO);
+        }
+        List<CommentDTO> commentDTOList = commentService.findAll(id);
+        if (commentDTOList.size() == 0) {
+            model.addAttribute("commentList", null);
+        } else {
+            model.addAttribute("commentList", commentDTOList);
+        }
+        return "boardPages/boardDetail";
+    }
+
+    @GetMapping("/update")
+    public String updateForm(@RequestParam("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "boardPages/boardUpdate";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findById(boardDTO.getId());
+        model.addAttribute("board", dto);
+//        return "redirect:/board?id="+boardDTO.getId();
+        return "boardPages/boardDetail";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
+        boardService.delete(id);
+        return "redirect:/board/list";
+    }
+
+
+
+
+
+
+
+
+
+
 }
